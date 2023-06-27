ea40ed7e1f4960eb673839079a1e9630225bb358
frank26080115 <frank.zhao.main@gmail.com>
2023-06-27 00:23:55 -0700

    roach heading manager wip

--- Roach/libraries/RoachHeadingManager/RoachHeadingManager.cpp
+++ Roach/libraries/RoachHeadingManager/RoachHeadingManager.cpp
@@ -1,2 +1,94 @@
 #include "RoachHeadingManager.h"
 
+RoachHeadingManager::RoachHeadingManager(uint32_t* p_timeout
+#ifdef RHEADMGR_USE_MODULES
+    ,nRF52RcRadio* r, RoachIMU* i, RoachPID* p
+#endif
+)
+{
+    #ifdef RHEADMGR_USE_MODULES
+    radio = r;
+    imu = i;
+    pid = p;
+    #endif
+    override_timeout = p_timeout;
+}
+
+bool RoachHeadingManager::task(roach_ctrl_pkt_t* pkt
+    #ifndef RHEADMGR_USE_MODULES
+        , bool force_reset, int32_t steering, float yaw, uint32_t session
+    #endif
+    )
+{
+    bool need_reset =
+        #ifdef RHEADMGR_USE_MODULES
+            false
+        #else
+            force_reset
+        #endif
+        ;
+
+    if (pkt->steering != 0)
+    {
+        need_reset = true;
+    }
+    else
+    {
+        if (prev_pkt.steering != 0)
+        {
+            release_timestamp = millis();
+        }
+
+        if ((millis() - release_timestamp) < (*override_timeout))
+        {
+            need_reset = true;
+        }
+    }
+
+    #ifdef RHEADMGR_USE_MODULES
+    if ((pkt->flags & ROACHPKTFLAG_GYROACTIVE) != 0) {
+        need_reset = true;
+    }
+    #endif
+
+    uint32_t sess = 
+        #ifdef RHEADMGR_USE_MODULES
+            radio->getSessionId()
+        #else
+            session
+        #endif
+            ;
+    uint32_t sess_diff = sess - prev_session_id;
+    if (sess_diff > 1) {
+        need_reset = true;
+    }
+    prev_session_id = sess;
+
+    memcpy((void*)&prev_pkt, (void*)pkt, sizeof(roach_ctrl_pkt_t));
+
+    #ifdef RHEADMGR_USE_MODULES
+    if (imu->getErrorOccured(true)) {
+        need_reset = true;
+    }
+    #endif
+
+    cur_head = lround(
+        #ifdef RHEADMGR_USE_MODULES
+        imu->euler.
+        #endif
+        yaw * 100.0);
+
+    if (need_reset)
+    {
+        #ifdef RHEADMGR_USE_MODULES
+        pid->reset();
+        #endif
+        angle_offset = pkt->heading - cur_head;
+    }
+
+    tgt_head = pkt->heading + angle_offset;
+    ROACH_WRAP_ANGLE(cur_head, 100);
+    ROACH_WRAP_ANGLE(tgt_head, 100);
+
+    return need_reset;
+}
--- Roach/libraries/RoachHeadingManager/RoachHeadingManager.h
+++ Roach/libraries/RoachHeadingManager/RoachHeadingManager.h
@@ -4,5 +4,49 @@
 #include <RoachLib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+//#define RHEADMGR_USE_MODULES
+
+#ifdef RHEADMGR_USE_MODULES
+#include <nRF52RcRadio.h>
+#include <RoachIMU.h>
+#include <RoachPID.h>
+#endif
+
+class RoachHeadingManager
+{
+    public:
+        RoachHeadingManager(uint32_t* p_timeout
+            #ifdef RHEADMGR_USE_MODULES
+                nRF52RcRadio* r, RoachIMU* i, RoachPID* p
+            #endif
+            );
+        bool task(roach_ctrl_pkt_t* pkt
+            #ifndef RHEADMGR_USE_MODULES
+                , bool force_reset, int32_t steering, float yaw, uint32_t session
+            #endif
+            );
+        inline int32_t getTgtHeading(void)  { return tgt_head; };
+        inline int32_t getCurHeading(void)  { return cur_head; };
+        inline int32_t getOffsetAngle(void) { return angle_offset; };
+
+    private:
+        #ifdef RHEADMGR_USE_MODULES
+        nRF52RcRadio* radio;
+        RoachIMU* imu;
+        RoachPID* pid;
+        #endif
+
+        uint32_t* override_timeout;
+
+        int32_t angle_offset;
+        int32_t cur_head;
+        int32_t tgt_head;
+        roach_ctrl_pkt_t prev_pkt;
+        uint32_t prev_session_id = 0;
+        uint32_t release_timestamp = 0;
+};
 
 #endif
